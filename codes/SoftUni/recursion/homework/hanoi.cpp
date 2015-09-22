c248195ac40f1fcb2db52f186fc813a8df60a523
AlexTs2002 <ostrietoxxx@gmail.com>
2015-09-22 14:18:57 +0300

    Codes

--- codes/SoftUni/recursion/homework/hanoi.cpp
+++ codes/SoftUni/recursion/homework/hanoi.cpp
@@ -7,21 +7,19 @@ void diskMove (unsigned n, char a, char b)
 	printf("Преместете диск %u от %c на %c.\n", n, a, b);
 }
 
-void hanoy (char a, char c, char b, unsigned numb)
+void hanoi (char a, char c, char b, unsigned numb)
 {
 	if (1 == numb)
 		diskMove(1, a, c);
 	else
 	{
-		hanoy(a, b, c, numb - 1);
+		hanoi(a, b, c, numb - 1);
 		diskMove(numb, a, c);
-		hanoy(b, c, a, numb - 1);
+		hanoi(b, c, a, numb - 1);
 	}
 }
 int main ()
 {
 	scanf ("%u", &n);
-	//printf ("Брой дискове: %u\n", n);
-	hanoy ('a', 'c', 'b', n);
-	return 0;
+	hanoi ('a', 'c', 'b', n);
 }
