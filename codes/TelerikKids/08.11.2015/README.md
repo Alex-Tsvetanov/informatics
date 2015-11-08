9118e89f36ea0377d2212a76d89d01e5892c7c01
AlexTs2002 <ostrietoxxx@gmail.com>
2015-11-08 13:14:24 +0200

    Codes

--- codes/TelerikKids/08.11.2015/README.md
+++ codes/TelerikKids/08.11.2015/README.md
@@ -4,5 +4,5 @@
 <br>
   * Дадена е редица от числа и число m. Намерете най-дългата подредица с разлика по абсолютна стойност между съседни елементи да е най-много m.
 <br>
-  * Моето решение се намира във файла [4. Big bad neighbours.cpp](https://github.com/Alex-Tsvetanov/Informatics/blob/master/codes/TelerikKids/01.11.2015/4.%20Big%20bad%20neighbours.cpp)
+  * Моето решение се намира във файла [task1.cpp](https://github.com/Alex-Tsvetanov/Informatics/blob/master/codes/TelerikKids/08.11.2015/task1.cpp)
 
--- codes/TelerikKids/08.11.2015/task1.cpp
+++ codes/TelerikKids/08.11.2015/task1.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <utility>
-
+#include <cmath>
 using namespace std;
 
 int n, m;
 int* input;
 
 pair <int, int>* dp;
+
+bool check (int ind1, int ind2)
+{
+	if (ind2 == 0 or ind1 == 0)
+		return true;
+	return ((-m <= (input [ind1] - input [ind2])) and ((input [ind1] - input [ind2]) <= m));
+}
 
 int main ()
 {
@@ -17,21 +24,19 @@ int main ()
 		cin >> input [i];
 //----------End of input --------------
 	dp = new pair <int, int> [n + 1];
-	dp [0] = {0, -1};
+	dp [0] = {0, 0};
 	for (int i = 1 ; i < n + 1 ; i ++)
 	{
 		int last_index = -1;
 		int maxLenght = -1;
-		for (int j = i - 1 ; j >= 0 ; j --)
-			if (dp [j].second > -1)
-			{
-				if (abs (input [i] - input [dp [j].second]) <= m)
+		for (int j = i - 1 ; j > 0 ; j --)
+			if (check (i, dp [j].second))
 				if (maxLenght < dp [j].first)
 				{
 					maxLenght = dp [j].first + 1;
 					last_index = i;
 				}
-			}
+
 		if (maxLenght < 1)
 			maxLenght = 1, last_index = i;
 		if (maxLenght < dp [i - 1].first)
