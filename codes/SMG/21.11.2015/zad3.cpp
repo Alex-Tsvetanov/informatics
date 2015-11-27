ebbdfcb96313c4a8cf0af49264d1233705569f75
AlexTs2002 <ostrietoxxx@gmail.com>
2015-11-27 08:24:12 +0200

    Codes

--- codes/SMG/21.11.2015/zad3.cpp
+++ codes/SMG/21.11.2015/zad3.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <vector>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,35 +11,121 @@ int num_of_digits (int N)
 	return ((N < 10) ? 1 : ((N < 100) ? 2 : ((N < 1000) ? 3 : ((N < 10000) ? 4 : ((N < 100000) ? 5 : ((N < 1000000) ? 6 : 7))))));
 }
 
-string f (int N)
+struct my_stack
 {
-	if (N == 1)
-		return "1";
-	stringstream ss;
-	string prev = f (N - 1);
-	ss << N << prev << prev;
-	return ss.str ();
+private:
+	stack < char > st;
+	int num_of_erased;
+public:
+	my_stack ()
+	{
+		st = stack < char > ();
+		num_of_erased = 0;
+	}
+
+	char push (char c, int k = -2)
+	{
+		if (!st.empty ())
+		{
+			if (c == st.top ())
+			{
+				st.pop ();
+				num_of_erased += 2;
+			}
+			else
+				st.push (c);
+		}
+		else
+			st.push (c);
+		if ((int)st.size () + num_of_erased == k or (int)st.size () + num_of_erased == k + 1)
+		{
+			return c;
+		}
+		else
+			return '\0';
+	}
+	void clear ()
+	{
+		num_of_erased = 0;
+		while (!st.empty ())
+			st.pop ();
+	}
+};
+
+my_stack str;
+
+int64_t k;
+
+char f_order (int64_t);
+char f_reversed (int64_t);
+
+char f_order (int64_t n)
+{
+	if (n == 1)
+	{
+		return str.push ('1', k);
+	}
+	string a;
+	{
+		stringstream ss;
+		ss << n;
+		a = ss.str ();
+	}
+	for (auto& x : a)
+	{
+		char c = str.push (x, k);
+		if ( c != '\0' )
+		{
+			return c;
+		}
+	}
+	char c;
+	c = f_order (n - 1);
+	if ( c != '\0' )
+		return c;
+	c = f_reversed (n - 1);
+	return c;
 }
 
-unsigned long long 
+char f_reversed (int64_t n)
+{
+	if (n == 1)
+	{
+		return str.push ('1', k);
+	}
+	string a;
+	{
+		stringstream ss;
+		ss << n;
+		a = ss.str ();
+		reverse (a.begin (), a.end ());
+	}
+	for (auto& x : a)
+	{
+		char c = str.push (x, k);
+		if ( c != '\0' )
+		{
+			return c;
+		}
+	}
+	char c;
+	c = f_reversed (n - 1);
+	if ( c != '\0' )
+		return c;
+	c = f_order (n - 1);
+	return c;
+}
 
 int main ()
 {
-	int N, K;
-	cin >> N >> K
-	for ( ; N > 1 ; N --)
-	{
-		if (K < num_of_digits (N))
-		{
-			stringstream ss;
-			ss << N;
-			cout << ss.str ()[K];
-			return 0;
-		}
-		else
-		{
-			K = (K - num_of_digits (N)) % lens [N - 1];
-		}
-	}
-	cout << 1 << endl;
+	int64_t N;
+	cin >> N >> k;
+	k ++;
+	cout << f_order (N) << endl;
+////for (k = 1 ; k <= ((1<<N) - 1) ; k ++)
+////{
+////	cout << f_order (N);
+////	cout.flush ();
+////	str.clear ();
+////}
 }
