10b923e1d7e5679c5e2b941dc5e0b865f7661373
AlexTs2002 <ostrietoxxx@gmail.com>
2016-03-13 10:09:22 +0200

    first commit

--- codes/my/TelerikKids/c++&algorithms/31.01.2016/pi.cpp
+++ codes/my/TelerikKids/c++&algorithms/31.01.2016/pi.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<iomanp>
+#include <iomanip>
 #include <random>
 
 using namespace std;
@@ -8,15 +8,17 @@ int main ()
 {
 	double avarage_pi = 0;
 	int maxN = 100;
+	cin >> maxN;
 	for (int n = 0 ; n < maxN ; n ++)
 	{
+		const long long blq = 1e6;
 		double pi = 0;
 		int br = 0;
 		std::random_device rd;
 		std::mt19937 gen (rd());
 		std::uniform_real_distribution<double> X (0, n + 1);
 		std::uniform_real_distribution<double> Y (0, n + 1);
-		for (int i = 0 ; i < 1000000 * (n + 1) ; i ++)
+		for (int i = 0 ; i < blq * (n + 1) ; i ++)
 		{
 			double x = X (gen);
 			double y = Y (gen);
@@ -24,7 +26,7 @@ int main ()
 			double radius = n + 1;
 			br += (dist <= radius);
 		}
-		pi = br / 1000000.0f;
+		pi = br / (float)blq;
 		pi *= 4;
 		avarage_pi += pi;
 		cout <<setprecision (1000)<< pi << " " << avarage_pi / (n + 1) << "\n";
