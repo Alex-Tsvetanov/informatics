4f5ed842e82f3e642768299c0e27f4b5d0fcf6f9
Alex Tsvetanov <ostrietoxxx@gmail.com>
2016-05-29 14:34:56 +0300

    new

--- codes/Alex/TelerikKids/c++&algorithms/29.05.2016/Alex/ball_game_AI_example.hpp
+++ codes/Alex/TelerikKids/c++&algorithms/29.05.2016/Alex/ball_game_AI_example.hpp
@@ -1,12 +1,12 @@
-struct
+struct AI_alex
 {
 	range my_range;
 	void choice (game_rules rules)
 	{
 		my_range = *rules.all_ranges.begin ();
 	}
-	int AI_alex (game_rules rules)
+	int turn (game_rules rules)
 	{
 		return *rules.rules.begin ();
 	}
-} AI_alex;
+};
--- codes/Alex/TelerikKids/c++&algorithms/29.05.2016/Alex/ball_game_core.cpp
+++ codes/Alex/TelerikKids/c++&algorithms/29.05.2016/Alex/ball_game_core.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 //#include <gtkmm.h>
 #include <set>
+#include <vector>
 #include <stdlib.h>
 
 using namespace std;
@@ -10,7 +11,7 @@ game_core game;
 
 int main (int argc, char *argv[])
 {
-	Gtk::Main app(argc, argv);
+	//Gtk::Main app(argc, argv);
 	game.set_rules (vector <int>{1,2,3,4}).set_balls (5);
 	AI_alex player1;
 	AI_alex player2;
@@ -18,9 +19,9 @@ int main (int argc, char *argv[])
 	player2.choice (game.get_rules ());
 	while (true)
 	{
-		int player1_turn = AI_alex (game.get_rules ());
-		int player2_turn = AI_alex (game.get_rules ());
+		int player1_turn = player1.turn (game.get_rules ());
 		game.doTurn (1, player1_turn);
+		int player2_turn = player2.turn (game.get_rules ());
 		game.doTurn (2, player2_turn);
 	}
 }
